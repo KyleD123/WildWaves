4479a5d9327a37f94f174f739dae1820469faf07
Kyle Doerksen <Kyledoerksen75@yahoo.ca>
2024-09-20 22:30:51 -0400

    Created WaveSpawner

---
+++ Assets/Scripts/WaveSpawner/WaveSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    public Transform[] spawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
